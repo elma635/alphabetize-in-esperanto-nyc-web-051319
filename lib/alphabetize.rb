25cf0f88d6781b648467c4f3176b7b49f17190f1
Elma Rahman <elmarahman635@gmail.com>
2019-05-05 01:36:20 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,10 @@
 require 'pry'
 
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
     arr.sort_by do |word|
       word.split('').map do |letter|
     ESPERANTO_ALPHABET.index(letter)
