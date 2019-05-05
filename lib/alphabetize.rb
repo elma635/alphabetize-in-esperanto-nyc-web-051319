5912fdc2ea5ec86b8c2c1fd6286b4a0adcf1517f
Elma Rahman <elmarahman635@gmail.com>
2019-05-05 01:23:27 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,9 +2,13 @@ require 'pry'
 
 def alphabetize(arr)
 arr.sort_by do |word|
-
-#binding.pry
+  word.split('').map do |letter|
+    alphabetize.index(letter)
+  end
+end
 end
 
 # - should use char
 # - should use map
+
+#alpha based on first  letter , second letter, sixth  letter, second letters not in English alphabet
